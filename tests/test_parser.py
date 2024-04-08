530f261c4aa8110d4f3e5218107dd8dbd8f4d105
Alexandre Hutton <50920802+AlexandreHutton@users.noreply.github.com>
2024-04-08 11:21:43 -0700

    updated command line arguments

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,13 +6,13 @@ import os
 
 
 class TestNodeParser(unittest.TestCase):
-    # def test_simplepackage(self):
-    #     tmp_f, tmp_name = tempfile.mkstemp(suffix="pscsapitest.json")
-    #     cur_dir = os.path.dirname(__file__)
-    #     parse_package(tmp_name,
-    #                   parse_directory=os.path.join(cur_dir, "sample_package", "loaders", "faster"),
-    #                   package_name="api_test")
-    #     return
+    def test_simplepackage(self):
+        tmp_f, tmp_name = tempfile.mkstemp(suffix="pscsapitest.json")
+        cur_dir = os.path.dirname(__file__)
+        parse_package(tmp_name,
+                      parse_directory=os.path.join(cur_dir, "sample_package", "loaders", "faster"),
+                      package_name="api_test")
+        return
 
     def test_parsepackage(self):
         tmp_f, tmp_name = tempfile.mkstemp(suffix="pscsapitest.json")
